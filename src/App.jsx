f95248bdfef1b6683c2dc9667f13bc7250ecdaf1
szvitek <gy.szvitek@gmail.com>
2024-04-04 14:14:19 +0100

    feat(App): split up the app into components

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
+import Card from './components/Card';
+
 function App() {
   return (
     <main>
-      <div>
-        <h1>Fancy Counter</h1>
-        <p>0</p>
-        <button>Reset</button>
-        <div>
-          <button>-</button>
-          <button>+</button>
-        </div>
-      </div>
+      <Card />
     </main>
   );
 }
