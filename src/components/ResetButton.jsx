e2a83c500675dbd2aacdece75e3010c75bc130aa
szvitek <gy.szvitek@gmail.com>
2024-04-04 17:22:30 +0100

    feat(ResetButton): Add a reset button to reset the count to 0

--- src/components/ResetButton.jsx
+++ src/components/ResetButton.jsx
@@ -1,7 +1,9 @@
 import { ResetIcon } from "@radix-ui/react-icons";
 
-export default function ResetButton() {
-  return <button className="reset-btn">
-    <ResetIcon className="reset-btn-icon" />
-  </button>;
+export default function ResetButton({ setCount }) {
+  return (
+    <button className="reset-btn" onClick={() => setCount(0)}>
+      <ResetIcon className="reset-btn-icon" />
+    </button>
+  );
 }
