efab55ba626b88925003d82b81cc7947e205e207
szvitek <gy.szvitek@gmail.com>
2024-04-04 17:17:59 +0100

    feat(State): Count State (useState, 'Rendering', 'Lifting state up', functional setState)

--- src/components/CountButtons.jsx
+++ src/components/CountButtons.jsx
@@ -1,12 +1,18 @@
-import { MinusIcon, PlusIcon } from "@radix-ui/react-icons";
+import { MinusIcon, PlusIcon } from '@radix-ui/react-icons';
 
-export default function CountButtons() {
+export default function CountButtons({ setCount }) {
   return (
     <div className="button-container">
-      <button className="count-btn">
+      <button
+        className="count-btn"
+        onClick={() => setCount((count) => count - 1)}
+      >
         <MinusIcon className="count-btn-icon" />
       </button>
-      <button className="count-btn">
+      <button
+        className="count-btn"
+        onClick={() => setCount((count) => count + 1)}
+      >
         <PlusIcon className="count-btn-icon" />
       </button>
     </div>
