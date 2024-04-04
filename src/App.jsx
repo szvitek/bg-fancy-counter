7f83a14981eb69957db790112c55fd25b5b6c190
szvitek <gy.szvitek@gmail.com>
2024-04-04 14:00:01 +0100

    feat(App): app structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
 function App() {
-  return <p>Hello</p>;
+  return (
+    <main>
+      <div>
+        <h1>Fancy Counter</h1>
+        <p>0</p>
+        <button>Reset</button>
+        <div>
+          <button>-</button>
+          <button>+</button>
+        </div>
+      </div>
+    </main>
+  );
 }
 
 export default App;
